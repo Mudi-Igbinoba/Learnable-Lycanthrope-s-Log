3e2e3b34a719be8248a0799341f4120c03b29794
mudi-igbinoba <ruth.igbinoba12@gmail.com>
2021-09-26 08:34:30 +0100

    initial and final commit

---
+++ journal.js
@@ -0,0 +1,269 @@
+// Making a constructor function for werewolf journal entries
+class JournalEntry{
+  constructor(arr, boolean) {
+    this.events = arr,
+    this.werewolf = boolean
+  }
+};
+
+// making journal entries for 3 months (91 days)
+let day1 = new JournalEntry(["carrot","exercise","weekend"], false);
+
+let day2 = new JournalEntry(["bread","pudding","brushed teeth","weekend","touched tree"], false);
+
+let day3 = new JournalEntry(["carrot","nachos","brushed teeth","cycling","weekend"], false);
+
+let day4 = new JournalEntry(["brussel sprouts","ice cream","brushed teeth","computer","weekend"], false);
+
+let day5 = new JournalEntry(["potatoes","candy","brushed teeth","exercise","weekend","dentist"], false);
+
+let day6 = new JournalEntry(["brussel sprouts","pudding","brushed teeth","running","weekend"], false);
+  
+let day7 = new JournalEntry(["pizza","brushed teeth","computer","work","touched tree"], false);
+
+let day8 = new JournalEntry(["bread","beer","brushed teeth","cycling","work"], false);
+
+let day9 = new JournalEntry(["cauliflower","brushed teeth","work"], false);
+
+let day10 = new JournalEntry(["pizza","brushed teeth","cycling","work"], false);
+
+let day11 = new JournalEntry(["lasagna","nachos","brushed teeth","work"], false);
+
+let day12 = new JournalEntry(["brushed teeth","weekend","touched tree"], false);
+
+let day13 = new JournalEntry(["lettuce","brushed teeth","television","weekend"], false);
+
+let day14 = new JournalEntry(["spaghetti","brushed teeth","work"], false);
+
+let day15 = new JournalEntry(["brushed teeth","computer","work"], false);
+
+let day16 = new JournalEntry(["lettuce","nachos","brushed teeth","work"], false);
+
+let day17 = new JournalEntry(["carrot","brushed teeth","running","work"], false);
+
+let day18 = new JournalEntry(["brushed teeth","work"], false);
+
+let day19 = new JournalEntry(["cauliflower","reading","weekend"], false);
+
+let day20 = new JournalEntry(["bread","brushed teeth","weekend"], false);
+
+let day21 = new JournalEntry(["lasagna","brushed teeth","exercise","work"], false);
+
+let day22 = new JournalEntry(["spaghetti","brushed teeth","reading","work"], false);
+
+let day23 = new JournalEntry (["carrot","ice cream","brushed teeth","television","work"], false);
+
+let day24 = new JournalEntry(["spaghetti","nachos","work"], false);
+
+let day25 = new JournalEntry(["cauliflower","ice cream","brushed teeth","cycling","work"], false);
+
+let day26 = new JournalEntry(["spaghetti","full moon", "stargazing","computer","weekend"], true);
+
+let day27 = new JournalEntry(["potatoes","ice cream","brushed teeth","computer","weekend"], false);
+
+let day28 = new JournalEntry(["potatoes","ice cream","brushed teeth","work"], false);
+
+let day29 = new JournalEntry(["peanuts","brushed teeth","running","work"], false);
+
+let day30 = new JournalEntry(["potatoes","exercise","work"], false);
+
+let day31 = new JournalEntry(["pizza","ice cream","computer","work"], false);
+
+let day32 = new JournalEntry(["lasagna","ice cream","work"], false);
+
+let day33 = new JournalEntry(["cauliflower","candy","reading","weekend"], false);
+
+let day34 = new JournalEntry(["lasagna","nachos","brushed teeth","running","weekend"], false);
+
+let day35 = new JournalEntry(["potatoes","brushed teeth","work"], false);
+
+let day36 = new JournalEntry(["carrot","work"], false);
+
+let day37 = new JournalEntry(["pizza","beer","work","dentist"], false);
+
+let day38 = new JournalEntry(["lasagna","pudding","cycling","work"], false);
+
+let day39 = new JournalEntry(["spaghetti","brushed teeth","reading","work"], false);
+
+let day40 = new JournalEntry(["spaghetti","pudding","television","weekend"], false);
+
+let day41 = new JournalEntry(["bread","brushed teeth","exercise","weekend"], false);
+
+let day42 = new JournalEntry(["lasagna","full moon", "stargazing","work"], true);
+
+let day43 = new JournalEntry(["pizza","work"], false);
+
+let day44 = new JournalEntry(["potatoes","exercise","work"], false);
+
+let day45 = new JournalEntry(["brushed teeth","exercise","work"], false);
+
+let day46 = new JournalEntry(["spaghetti","brushed teeth","television","work"], false);
+
+let day47 = new JournalEntry(["pizza","cycling","weekend"], false);
+
+let day48 = new JournalEntry(["carrot","brushed teeth","weekend"], false);
+
+let day49 = new JournalEntry(["carrot","beer","brushed teeth","work"], false);
+
+let day50 = new JournalEntry(["pizza","full moon", "stargazing","candy","work"], true);
+
+let day51 = new JournalEntry(["carrot","peanuts","brushed teeth","reading","work"], false);
+
+let day52 = new JournalEntry(["potatoes","peanuts","brushed teeth","work"], false);
+
+let day53 = new JournalEntry(["carrot","nachos","brushed teeth","exercise","work"], false);
+
+let day54 = new JournalEntry(["pizza","peanuts","brushed teeth","television","weekend"], false);
+
+let day55 = new JournalEntry(["lasagna","brushed teeth","cycling","weekend"], false);
+
+let day56 = new JournalEntry(["rice", "beans"], false);
+
+let day57 = new JournalEntry(["cauliflower","peanuts","brushed teeth","computer","work","touched tree"], false);
+
+let day58 = new JournalEntry(["lettuce","brushed teeth","television","work"], false);
+
+let day59 = new JournalEntry(["potatoes","brushed teeth","computer","work"], false);
+
+let day60 = new JournalEntry(["bread","candy","work"], false);
+
+let day61 = new JournalEntry(["potatoes","nachos","work"], false);
+
+let day62 = new JournalEntry(["carrot","pudding","brushed teeth","weekend"], false);
+
+let day63 = new JournalEntry(["carrot","brushed teeth","exercise","weekend","touched tree"], false);
+
+let day64 = new JournalEntry(["brussel sprouts","running","work"], false);
+
+let day65 = new JournalEntry(["brushed teeth","work"], false);
+
+let day66 = new JournalEntry(["lettuce","brushed teeth","running","work"], false);
+
+let day67 = new JournalEntry(["candy","brushed teeth","work"], false);
+
+let day68 = new JournalEntry(["brussel sprouts","brushed teeth","computer","work"], false);
+
+let day69 = new JournalEntry(["bread","brushed teeth","weekend"], false);
+
+let day70 = new JournalEntry(["cauliflower","brushed teeth","weekend"], false);
+
+let day71 = new JournalEntry(["spaghetti","candy","television","work","touched tree"], false);
+
+let day72 = new JournalEntry(["carrot","pudding","brushed teeth","work"], false);
+
+let day73 = new JournalEntry(["lettuce","brushed teeth","work"], false);
+
+let day74 = new JournalEntry(["carrot","ice cream","brushed teeth","cycling","work"], false);
+
+let day75 = new JournalEntry(["pizza","brushed teeth","work"], false);
+
+let day76 = new JournalEntry(["spaghetti","full moon", "stargazing","exercise","weekend"], true);
+
+let day77 = new JournalEntry(["bread","beer","computer","weekend","touched tree"], false);
+
+let day78 = new JournalEntry(["brushed teeth","running","work"], false);
+
+let day79 = new JournalEntry(["lettuce","peanuts","brushed teeth","work","touched tree"], false);
+
+let day80 = new JournalEntry(["lasagna","brushed teeth","television","work"], false);
+
+let day81 = new JournalEntry(["cauliflower","brushed teeth","running","work"], false);
+
+let day82 = new JournalEntry(["carrot","brushed teeth","running","work"], false);
+
+let day83 = new JournalEntry(["carrot","reading","weekend"], false);
+
+let day84 = new JournalEntry(["carrot","full moon", "stargazing","reading","weekend"], true);
+
+let day85 = new JournalEntry(["potatoes","brushed teeth","running","work"], false);
+
+let day86 = new JournalEntry(["lasagna","ice cream","work","touched tree"], false);
+
+let day87 = new JournalEntry(["cauliflower","peanuts","brushed teeth","cycling","work"], false);
+
+let day88 = new JournalEntry(["pizza","brushed teeth","running","work"], false);
+
+let day89 = new JournalEntry(["lettuce","brushed teeth","work"], false);
+
+let day90 = new JournalEntry(["bread","brushed teeth","television","weekend"], false);
+
+let day91 = new JournalEntry(["cauliflower","peanuts","brushed teeth","weekend"], false)
+
+// putting all journal entries into an array
+var JOURNAL = [day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25, day26, day27, day28, day29, day30, day31, day32, day33, day34, day35, day36, day37, day38, day39, day40, day41, day42, day43, day44, day45, day46, day47, day48, day49, day50, day51, day52, day53, day54, day55, day56, day57, day58, day59, day60, day61, day62, day63, day64, day65, day66, day67, day68, day69, day70, day71, day72, day73, day74, day75, day76, day77, day78, day79, day80, day81, day82, day83, day84, day85, day86, day87, day88, day89, day90, day91];
+
+console.log(JOURNAL);
+
+
+//finding all journal entries where he turned into a werewolf and pushing it into an array
+let werewolfTrue = []
+
+for(let i = 0; i < JOURNAL.length; i++){
+  let property = JOURNAL[i]
+  if(property.werewolf === true){
+    werewolfTrue.push(property)
+  }
+}
+ 
+console.log(werewolfTrue);
+
+// pushing all object property values into an array
+let entries = []
+for(let i = 0; i < werewolfTrue.length; i++){
+  let property = werewolfTrue[i]
+  entries.push(Object.values(property))
+}
+
+
+//removing the true value from all sub arrays within entries array, leaving the events property values
+for(let i = 0; i < werewolfTrue.length; i++){
+  let property = entries[i]
+  for(let j = 0; j < property.length; j++){
+  if(property[j] === true){
+    property.pop();
+
+    }
+  }
+}
+let activityEntries = entries
+console.log(activityEntries);
+
+//flattening or expanding all sub arrays within activityEntries
+let activities = activityEntries.flat(Infinity)
+ 
+console.log(activities)
+
+//finding the common event that made him turn into a werewolf
+function mode(events) {
+  var modes =[];
+  var count = [];
+  var i;
+  var event;
+  var maxIndex = 0
+
+  for(i = 0; i < events.length; i += 1){
+    event = events[i]
+    count[event] = (count[event] || 0) + 1;
+
+    if(count[event] > maxIndex){
+      maxIndex = count[event]
+    }
+  }
+
+  for(i in count){
+    if (count.hasOwnProperty(i)) {
+      if (count[i] === maxIndex){
+        modes.push(String(i))
+      }
+    }
+  }
+
+  return modes;
+}
+
+let commonWerewolfTriggers = mode(activities)
+
+console.log(commonWerewolfTriggers)
+
+//The events that were common for all the days he turned into a werewolf was stargazing and full moon.
+//Therefore full moon and stargazing was what made him turn into a werewolf
